869fab0f8b3ce393520eeb401280e6b0388b0db8
Ezra Nzioki <135170744+Ezra-nzioki@users.noreply.github.com>
2025-11-03 11:58:10 +0000

    added responsive nav

--- app/src/components/layout/nav.jsx
+++ app/src/components/layout/nav.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Nav = () => {
+  const [open,setOpen]=useState(false)
   return (
  <>
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
