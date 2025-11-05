22328f2314fb43d21dc91523f33853c2a245043d
Ezra Nzioki <135170744+Ezra-nzioki@users.noreply.github.com>
2025-11-05 23:50:41 +0000

    created about section

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,4 @@
 
-import './App.css'
 import { Header } from './components/layout/header'
 import { Hero } from './components/sections/hero'
 
--- app/src/components/common/btn.jsx
+++ app/src/components/common/btn.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const Btn = () => {
+export const Btn = (props) => {
+  const { text } = props  
   return (
     <div class="flex justify-center">
         <button class="mt-6 px-5 py-1 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition ">
-            shop now
+          {text}
         </button>
     </div>
   )
